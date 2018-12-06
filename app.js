060a7aa5d815d283dba82fa9f90e56f3c055e214
Dobrin, Michael <michael.dobrin@rakuten.com>
2018-12-06 13:25:06 +0900

    s

--- app.js
+++ app.js
@@ -1,40 +1,33 @@
-// test
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('index.html');
+const http = require('http');
+var querystring = require('querystring');
+const request = require('request')
 
-var log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
 
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
 
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
+var koSanOutput = ''
+request.get('https://blooming-fortress-29641.herokuapp.com/testtest',
+ (error, res, body) => {
+  if (error) {
+    console.log('second request');
+    console.error(error)
+    return
+  }
+  console.log(`statusCode: ${res.statusCode}`)
+  console.log('successful second request')
+  console.log(body)
+  koSanOutput = body
+})
 
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
 
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
+ hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(koSanOutput);
 });
 
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
-
-// Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
