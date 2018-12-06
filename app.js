f706421299c2c0b4596757290bfe4dc88895117e
Dobrin, Michael <michael.dobrin@rakuten.com>
2018-12-06 13:29:43 +0900

    t

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
-const http = require('http');
-var querystring = require('querystring');
 const request = require('request')
 
+// test
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('index.html');
 
+var log = function(entry) {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
 
-var koSanOutput = ''
-request.get('https://blooming-fortress-29641.herokuapp.com/testtest',
- (error, res, body) => {
-  if (error) {
-    console.log('second request');
-    console.error(error)
-    return
-  }
-  console.log(`statusCode: ${res.statusCode}`)
-  console.log('successful second request')
-  console.log(body)
-  koSanOutput = body
-})
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
 
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
 
- hostname = '127.0.0.1';
-const port = 3000;
+        req.on('end', function() {
+            if (req.url === '/') {
+                log('Received message: ' + body);
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(koSanOutput);
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        res.writeHead(200);
+        res.write(html);
+        res.end();
+    }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
