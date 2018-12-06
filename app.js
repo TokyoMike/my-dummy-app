545e623c1fda799487c849e90164d2ac5ce8dbb2
Dobrin, Michael <michael.dobrin@rakuten.com>
2018-12-06 13:54:40 +0900

    c

--- app.js
+++ app.js
@@ -26,30 +26,37 @@ var log = function(entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
 };
 
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
+//var server = http.createServer(function (req, res) {
+//    if (req.method === 'POST') {
+//        var body = '';
+//
+//        req.on('data', function(chunk) {
+//            body += chunk;
+//        });
+//
+ //       req.on('end', function() {
+//            if (req.url === '/') {
+//                log('Received message: ' + body);
+//            } else if (req.url = '/scheduled') {
+//               log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+//           }
+//
+//            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+//            res.end();
+//        });
+//    } else {
+//        res.writeHead(200);
+//        res.write(html);
+//        res.end();
+//    }
+//});
 
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
-
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
+var server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(koSanOutput);
 });
+
 
 // Listen on port 3000, IP defaults to 127.0.0.1
 server.listen(port);
